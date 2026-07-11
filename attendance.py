eb5426f146aa764aa7145142db0723e3dbf72392
zeeerwaz4-coder <zeeerwaz4@gmail.com>
2026-07-12 00:15:47 +0800

    Add attendance data for employees

--- attendance.py
+++ attendance.py
@@ -1,1 +1,14 @@
-
+attendance = [
+    {
+        "employee_id": "EMP001",
+        "days_worked": 20,
+        "overtime_hours": 5,
+        "late_hours": 2
+    },
+    {
+        "employee_id": "EMP002",
+        "days_worked": 22,
+        "overtime_hours": 3,
+        "late_hours": 0
+    }
+]
